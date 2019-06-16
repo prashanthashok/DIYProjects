41b71dbca2351f0b984ba4005feda0d2848f248d
prashanthashok <prashanth.ashokram@gmail.com>
2019-06-15 20:21:58 -0400

    Search and Replace

--- FreeCodeCamp Projects/palindrome-checker.js
+++ FreeCodeCamp Projects/palindrome-checker.js
@@ -1,9 +1,15 @@
 function palindrome(str) {
-  // Good luck!
+  //only extract alphanumeric characters
   var arr = str.match(/[a-z0-9]+/ig);
+
   console.log(arr)
+  //join all characters and convert to lowercase
   var orig = arr.join('').toLowerCase();
+
+  //reverse string
   var reverse = orig.split('').reverse().join('');
+
+  //If true -> Palindrome
   if(orig === reverse) return true;
   else return false;
 }

d04209b05b6822f9c977fc0a06d57a30f67a7bba
prashanthashok <prashanth.ashokram@gmail.com>
2019-07-14 18:53:01 -0400

    #30 - Handing POST requests

--- node-js/net-ninja-tutorial/app.js
+++ node-js/net-ninja-tutorial/app.js
@@ -1,9 +1,12 @@
 //---------------------//
-//#29 - Query strings //
+//#30 - Handling POST requests //
 //---------------------//
 
 var express = require('express');
+var bodyParser = require('body-parser'); //to handle POST requests
 var app = express();
+
+var urlencodedParser = bodyParser.urlencoded({extended: false})
 
 app.set('view engine', 'ejs'); //whenever a view is requested, it looks into /views folder
 // app.use('/assets', (req, res, next)=>{ //next parameter is used to indicate nodejs to go on to the next middleware
@@ -21,6 +24,10 @@ app.get('/contact', (req, res) => {
     res.render('contact', {qs: req.query});
 });
 
+app.post('/contact', urlencodedParser, (req, res) => {
+    console.log(req.body);
+    res.render('contact-success', {data: req.body});
+});
 
 app.get('/profile/:name', (req, res) => {
     var data = {age: 28, job: 'ninja', hobbies: ['eating', 'fighting', 'coding']};

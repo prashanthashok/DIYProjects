a3331eb994840a6873fe91b9a2c0a6f38420f8d9
prashanthashok <prashanth.ashokram@gmail.com>
2019-07-14 17:18:59 -0400

    #20-22 - NPM

--- node-js/net-ninja-tutorial/app.js
+++ node-js/net-ninja-tutorial/app.js
@@ -1,34 +1,10 @@
 //---------------------//
-//#19 - Basic Routing//
+//#20-22 - NPM//
 //---------------------//
 
-var http = require('http');
-var fs = require('fs');
 
-
-//myReadStream.pipe(myWriteStream); //can only be used pipe on readable streams
-
-
-var server = http.createServer((req, res)=>{
-    console.log('request was made: ' + req.url);
-    if(req.url === '/home' || req.url === '/'){
-        res.writeHead(200, {'Content-Type':'text/html'});
-        fs.createReadStream(__dirname + '/index.html').pipe(res);
-    } else if(req.url === '/contact'){
-        res.writeHead(200, {'Content-Type':'text/html'});
-        fs.createReadStream(__dirname + '/contact.html').pipe(res);
-    } else if(req.url === '/api/ninjas'){
-        var ninjas = [{name: 'Fizz', age: '28'}, {name: 'Yoshi', age: 32}];
-        res.writeHead(200, {'Content-Type':'application/json'});
-        res.end(JSON.stringify(ninjas));
-    } else{
-        res.writeHead(404, {'Content-Type':'text/html'});
-        fs.createReadStream(__dirname + '/404.html').pipe(res);
-    }
-    
-    
-
-});
-
-server.listen(3000, '127.0.0.1');
-console.log('Listening on port 3000');
+//npm init //to initialize package.json
+//npm install <package-name> -save //saves the package as a dependency in package.json
+//npm uninstall //uninstalls package //also removes the package from package.json possible bug?
+//nodemon //Monitors changes in file and automatically restarts the server
+//nodemon app.js //Starts the server and listens for any changes to any files
